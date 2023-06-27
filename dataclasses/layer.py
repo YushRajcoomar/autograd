1d3d1befb19c8787eba2e3eafb01fa65a5f2f5ea
YushRajcoomar <yush2398@live.com>
2023-06-26 22:58:05 -0400

    MLP complete

---
+++ dataclasses/layer.py
@@ -0,0 +1,12 @@
+from neuron import Neuron
+
+class Layer:
+    def __init__(self,nin,nouts:list) -> None:
+        self.neurons = [Neuron(nin) for _ in range(nouts)]
+
+    def __call__(self,x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
---
+++ dataclasses/neuron.py
@@ -0,0 +1,18 @@
+from value import Value
+
+import numpy as np
+
+class Neuron:
+    def __init__(self,nin) -> None:
+        self.nin = nin
+        self.w = [Value(np.random.uniform(-1,1),label=f'w_{i}') for i,_ in enumerate(range(self.nin))]
+        self.b = Value(np.random.uniform(-1,1),label='b')
+
+    def __call__(self, x):
+        # tanh(w*x +b)
+        act = sum((wi*xi for wi,xi in zip(self.w,x)), self.b)
+        out = act.tanh()
+        return out
+    
+    def parameters(self):
+        return self.w + [self.b]
